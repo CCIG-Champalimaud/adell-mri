7e3d1951ce61ea9972ad7b0796a26266f67b42b9
josegcpa <jose.almeida@research.fchampalimaud.org>
2022-05-30 11:52:10 +0100

    added scripts relevant for ahnet training

---
+++ ahnet-train-prostate-x.py
@@ -0,0 +1,411 @@
+import argparse
+import random
+import yaml
+import numpy as np
+import nibabel as nib
+import torch
+import monai
+from sklearn.model_selection import KFold,train_test_split
+from torchmetrics import JaccardIndex,Precision,FBetaScore
+from tqdm import tqdm
+
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import EarlyStopping,StochasticWeightAveraging
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
+
+from lib.utils import (
+    loss_factory,
+    activation_factory,
+    get_prostatex_path_dictionary,
+    get_size_spacing_dict,
+    get_loss_param_dict,
+    collate_last_slice,
+    ConvertToOneHot,
+    RandomSlices,
+    SlicesToFirst)
+from lib.modules.segmentation_pl import AHNetPL
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    # data
+    parser.add_argument(
+        '--base_path',dest='base_path',type=str,
+        help="Path to ProstateX dataset",required=True)
+    parser.add_argument(
+        '--mod',dest='mod',type=str,choices=["T2WAx","DWI"],
+        help="Key to be used",required=True)
+    parser.add_argument(
+        '--classes',dest='classes',type=str,nargs='+',
+        choices=["gland","lesion"],
+        help="Classes to be considered",required=True)
+    # network + training
+    parser.add_argument(
+        '--config_file',dest="config_file",
+        help="Path to network configuration file (yaml)",
+        required=True)
+    
+    # training
+    parser.add_argument('--dev',dest='dev',
+        help="Device for PyTorch training",choices=["cuda","cpu"],type=str)
+    parser.add_argument(
+        '--seed',dest='seed',help="Random seed",default=42,type=int)
+    parser.add_argument(
+        '--n_workers',dest='n_workers',
+        help="No. of workers",default=1,type=int)
+    parser.add_argument(
+        '--augment',dest='augment',action="store_true",
+        help="Use data augmentations",default=False)
+    parser.add_argument(
+        '--downsample_rate',dest='downsample_rate',type=float,
+        default=1.0,help="Resizes images with downsample rate")
+    parser.add_argument(
+        '--loss_gamma',dest="loss_gamma",
+        help="Gamma for focal loss",default=2.0,type=float)
+    parser.add_argument(
+        '--loss_comb',dest="loss_comb",
+        help="Relative weight for combined losses",default=0.5,type=float)
+    parser.add_argument(
+        '--max_epochs',dest="max_epochs",
+        help="Maximum number of training epochs",default=100,type=int)
+    parser.add_argument(
+        '--n_folds',dest="n_folds",
+        help="Number of validation folds",default=5,type=int)
+    parser.add_argument(
+        '--checkpoint_dir',dest='checkpoint_dir',type=str,default="models",
+        help='Path to directory where checkpoints will be saved.')
+    parser.add_argument(
+        '--checkpoint_name',dest='checkpoint_name',type=str,default="model_x",
+        help='Checkpoint ID.')
+    parser.add_argument(
+        '--summary_dir',dest='summary_dir',type=str,default="summaries",
+        help='Path to summary directory (for tensorboard).')
+    parser.add_argument(
+        '--summary_name',dest='summary_name',type=str,default="model_x",
+        help='Summary name.')
+    parser.add_argument(
+        '--metric_path',dest='metric_path',type=str,default="metrics.csv",
+        help='Path to file with CV metrics + information.')
+    parser.add_argument(
+        '--early_stopping',dest='early_stopping',type=int,default=None,
+        help="No. of checks before early stop (defaults to no early stop).")
+    parser.add_argument(
+        '--swa',dest='swa',action="store_true",
+        help="Use stochastic gradient averaging.",default=False)
+    parser.add_argument(
+        '--checkpoint_restore','checkpoint_restore',type=str,default=None,
+        help="Path to checkpoint that will be restored prior to training.")
+    parser.add_argument(
+        '--spatial_dim_training','spatial_dim_training',type=int,default=2,
+        help="Spatial dimension during training.")
+
+    args = parser.parse_args()
+
+    torch.manual_seed(args.seed)
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    g = torch.Generator()
+    g.manual_seed(args.seed)
+
+    output_file = open(args.metric_path,'w')
+
+    args.classes = sorted(args.classes)
+    if len(args.classes) == 1:
+        n_classes = 2
+    elif len(args.classes) == 2:
+        n_classes = 3
+
+    keys = [args.mod]
+    label_keys = ["{}_{}_segmentations".format(args.mod,c)
+                  for c in args.classes]
+    all_keys = [args.mod,*label_keys]
+
+    path_dictionary = get_prostatex_path_dictionary(args.base_path)
+    fpd = {}
+    for pid in path_dictionary:
+        if all([k in path_dictionary[pid] for k in all_keys]):
+            fpd[pid] = {k:path_dictionary[pid][k] for k in all_keys}
+    path_dictionary = fpd
+    size_dict,spacing_dict = get_size_spacing_dict(path_dictionary,[args.mod])
+
+    for k in size_dict:
+        mm = np.median(np.array(size_dict[k]),axis=0)
+        # downsample only non-depth dimensions
+        mm[0] = mm[0]*args.downsample_rate
+        mm[1] = mm[1]*args.downsample_rate
+        size_dict[k] = np.int32(np.round(mm))
+        spacing_dict[k] = np.median(np.array(spacing_dict[k]),axis=0)
+
+    with open(args.config_file,'r') as o:
+        network_config = yaml.safe_load(o)
+
+    if "activation_fn" in network_config:
+        network_config["activation_fn"] = activation_factory[
+            network_config["activation_fn"]]
+
+    if "loss_fn" in network_config:
+        loss_key = network_config["loss_fn"]
+        k = "binary" if n_classes == 2 else "categorical"
+        network_config["loss_fn"] = loss_factory[k][
+            network_config["loss_fn"]]
+
+    if "spatial_dimensions" not in network_config:
+        network_config["spatial_dimensions"] = 3
+
+    if "batch_size" not in network_config:
+        network_config["batch_size"] = 1
+
+    print("Setting up transforms...")
+    if args.augment == True:
+        augments = [
+            monai.transforms.RandBiasFieldd([args.mod]),
+            monai.transforms.RandGaussianNoised([args.mod]),
+            monai.transforms.RandGibbsNoised([args.mod]),
+            monai.transforms.RandAdjustContrastd([args.mod])]
+    else:
+        augments = []
+
+    intp = ["area","nearest","nearest"][:n_classes]
+    transforms_train = [
+        monai.transforms.LoadImaged(all_keys),
+        monai.transforms.AddChanneld(all_keys),
+        monai.transforms.Orientationd(all_keys,"RAS"),
+        monai.transforms.EnsureTyped(all_keys),
+        # in case images should be downsampled
+        monai.transforms.Resized(
+            all_keys,tuple(size_dict[args.mod]),mode=intp),
+        *augments,
+        monai.transforms.ScaleIntensityd([args.mod],0,1),
+        monai.transforms.EnsureTyped(all_keys)]
+
+    transforms_train_val = [
+        monai.transforms.LoadImaged(all_keys),
+        monai.transforms.AddChanneld(all_keys),
+        monai.transforms.Orientationd(all_keys,"RAS"),
+        monai.transforms.EnsureTyped(all_keys),
+        # in case images should be downsampled
+        monai.transforms.Resized(
+            all_keys,tuple(size_dict[args.mod]),mode=intp),
+        monai.transforms.ScaleIntensityd([args.mod],0,1),
+        monai.transforms.EnsureTyped(all_keys)]
+
+    transforms_val = [
+        monai.transforms.LoadImaged(all_keys),
+        monai.transforms.AddChanneld(all_keys),
+        monai.transforms.Orientationd(all_keys,"RAS"),
+        monai.transforms.EnsureTyped(all_keys),
+        monai.transforms.Resized(
+            all_keys,tuple(size_dict[args.mod]),mode=intp),
+        monai.transforms.ScaleIntensityd([args.mod],0,1),
+        monai.transforms.EnsureTyped(all_keys)]
+
+    if n_classes > 2:
+        lesion_key = "{}_{}_segmentations".format(args.mod,"lesion")
+        label_key = "label"
+        transforms_train.append(
+            ConvertToOneHot(label_keys,label_key,lesion_key,bg=True))
+        transforms_train_val.append(
+            ConvertToOneHot(label_keys,label_key,lesion_key,bg=True))
+        transforms_val.append(
+            ConvertToOneHot(label_keys,label_key,lesion_key,bg=True))
+    else:
+        label_key = "{}_{}_segmentations".format(args.mod,args.classes[0])
+
+    if network_config["spatial_dimensions"] == 2:
+        transforms_train.append(
+            RandomSlices([args.mod,label_key],label_key,4,base=0.01))
+        transforms_train_val.append(
+            RandomSlices([args.mod,label_key],label_key,4,base=0.01))
+        transforms_val.append(
+            SlicesToFirst([args.mod,label_key]))
+        collate_fn = collate_last_slice
+    else:
+        collate_fn = None
+
+    all_pids = [k for k in path_dictionary]
+    if args.n_folds > 1:
+        fold_generator = KFold(
+            args.n_folds,shuffle=True,random_state=args.seed).split(all_pids)
+    else:
+        fold_generator = iter(
+            [train_test_split(range(len(all_pids)),test_size=0.2)])
+
+    for val_fold in range(args.n_folds):
+        train_idxs,val_idxs = next(fold_generator)
+        train_pids = [all_pids[i] for i in train_idxs]
+        val_pids = [all_pids[i] for i in val_idxs]
+        path_list_train = [path_dictionary[pid] for pid in train_pids]
+        path_list_val = [path_dictionary[pid] for pid in val_pids]
+
+        train_dataset = monai.data.CacheDataset(
+            path_list_train,monai.transforms.Compose(transforms_train))
+        train_dataset_val = monai.data.CacheDataset(
+            path_list_train,monai.transforms.Compose(transforms_train_val))
+        validation_dataset = monai.data.CacheDataset(
+            path_list_val,monai.transforms.Compose(transforms_val))
+
+        print("Calculating class weights...")
+        if n_classes == 2:
+            label_key = label_keys[0]
+            positive = 0.
+            total = 0.
+            for element in path_list_train:
+                if label_key in element:
+                    path = element[label_key]
+                    x = nib.load(path).get_fdata()
+                    positive += x.sum()
+                    total += x.size
+            
+            print("Class proportions: {:.3f}% ({})".format(
+                positive/total*100,args.classes[0]))
+            weights = total/(2*positive)
+            weights = torch.as_tensor([weights])
+
+        else:
+            counts = {"positive":[0 for _ in args.classes],
+                    "total":0}
+            N = 0
+            for element in path_list_train:
+                for i,kk in enumerate(label_keys):
+                    if kk in element:
+                        N += 1
+                        path = element[kk]
+                        x = nib.load(path).get_fdata()
+                        counts["positive"][i] += x.sum()
+                        size = x.size
+                counts["total"] += size
+
+            weights =  np.array([
+                counts["total"]/(3*(counts["total"]-np.sum(counts["positive"]))),
+                counts["total"]/(3*counts['positive'][0]),
+                counts["total"]/(3*counts['positive'][1])])
+            print("Class proportions: {:.3f}% (gland), {:.3f}% (lesion)".format(
+                counts["positive"][0]/counts["total"]*100,
+                counts["positive"][1]/counts["total"]*100))
+            weights = torch.as_tensor(weights)
+            weights = weights/torch.sum(weights)
+        
+        weights = weights.to(args.dev)
+        
+        loss_params = get_loss_param_dict(
+            weights=weights,gamma=args.loss_gamma,
+            comb=args.loss_comb)[loss_key]
+
+        train_loader_call = lambda: monai.data.ThreadDataLoader(
+            train_dataset,batch_size=network_config["batch_size"],
+            shuffle=True,num_workers=args.n_workers,generator=g,
+            collate_fn=collate_fn,pin_memory=True,
+            persistent_workers=True)
+
+        train_loader = train_loader_call()
+        train_val_loader = monai.data.ThreadDataLoader(
+            train_dataset_val,batch_size=network_config["batch_size"],
+            shuffle=False,num_workers=args.n_workers,generator=g,
+            collate_fn=collate_fn,persistent_workers=True)
+        validation_loader = monai.data.ThreadDataLoader(
+            validation_dataset,batch_size=1,
+            shuffle=False,num_workers=args.n_workers,generator=g,
+            collate_fn=collate_fn,persistent_workers=True)
+
+        print("Setting up training...")
+        ahnet = AHNetPL(training_dataloader_call=train_loader_call,
+                          image_key=keys[0],label_key=label_key,
+                          loss_params=loss_params,n_classes=n_classes,
+                          **network_config)
+        if args.checkpoint_restore is not None:
+            state_dict = torch.load(
+                args.checkpoint_restore,map_location=args.dev)['state_dict']
+            ahnet.load_state_dict(state_dict)
+        if args.spatial_dim_training != ahnet.spatial_dim:
+            if ahnet.spatial_dim == 3:
+                raise Exception("Can only convert from 2D to 3D")
+            else:
+                ahnet.convert_to_3d()
+        
+        callbacks = []
+
+        if args.early_stopping is not None:
+            early_stopping = EarlyStopping(
+                'val_loss',patience=args.early_stopping,
+                strict=True,mode="min")
+            callbacks.append(early_stopping)
+        if args.swa == True:
+            swa_callback = StochasticWeightAveraging()
+            callbacks.append(swa_callback)
+
+        ckpt_name = args.checkpoint_name + "_fold" + str(val_fold)
+        ckpt_name = ckpt_name + "_{epoch:03d}"
+        ckpt_name = ckpt_name + "_{val_loss:.3f}"
+        ckpt_callback = ModelCheckpoint(
+            dirpath=args.checkpoint_dir,
+            filename=ckpt_name,monitor="val_loss",
+            save_last=True,save_top_k=2,mode="min")
+        ckpt_callback.CHECKPOINT_NAME_LAST = \
+            args.checkpoint_name + "_fold" + str(val_fold) + "_last"
+        callbacks.append(ckpt_callback)
+        logger = TensorBoardLogger(
+                save_dir=args.summary_dir, 
+                name="{}_fold{}".format(args.summary_name,val_fold))
+
+        trainer = Trainer(
+            accelerator="gpu" if args.dev=="cuda" else "cpu",devices=1,
+            logger=logger,callbacks=callbacks,
+            auto_lr_find=True,max_epochs=args.max_epochs,
+            check_val_every_n_epoch=1,log_every_n_steps=50)
+
+        trainer.fit(unet, train_loader, train_val_loader)
+
+        # assessing performance on validation set
+        unet = unet.to(args.dev)
+        unet.eval()
+        if n_classes > 2:
+            metrics = {
+                "iou":JaccardIndex(n_classes,reduction="none"),
+                "prec":Precision(
+                    n_classes,mdmc_average="samplewise",average="none"),
+                "f1-score":FBetaScore(
+                    n_classes,mdmc_average="samplewise",average="none")}
+        else:
+            metrics = {
+                "iou":JaccardIndex(n_classes),
+                "prec":Precision(None),
+                "f1-score":FBetaScore(None,average="micro")}
+            jaccard_validation = JaccardIndex(n_classes)
+            precision_validation = Precision(None)
+        
+        metrics = {k:metrics[k].to(args.dev) for k in metrics}
+
+        print("Validating...")
+        for s in tqdm(validation_loader):
+            pred = unet(s[args.mod].to(args.dev))
+            y = torch.squeeze(s[label_key],1).to(args.dev)
+            try: y = torch.round(y).int()
+            except: pass
+
+            for k in metrics:
+                metrics[k].update(pred,y)
+
+        for k in metrics:
+            metrics[k] = metrics[k].compute().to("cpu")
+        
+        if n_classes > 2:
+            # multi class scenario
+            for k in metrics:
+                for i,v in enumerate(metrics[k]):
+                    S = "{},{},{},{}".format(
+                        k,val_fold,i,float(v.numpy()))
+                    print(S)
+                    output_file.write(S+'\n')
+                
+        else:
+            for k in metrics:
+                S = "{},{},{},{}".format(
+                    k,val_fold,0,float(metrics[k].numpy()))
+                print(S)
+                output_file.write(S+'\n')
+        
+        output_file.write("{},{},{},{}\n".format(
+            "train_ids",val_fold,0,":".join(train_pids)))
+        output_file.write("{},{},{},{}\n".format(
+            "val_ids",val_fold,0,":".join(val_pids)))
--- lib/modules/segmentation_pl.py
+++ lib/modules/segmentation_pl.py
@@ -179,3 +179,178 @@ class UNetPL(pl.LightningModule,UNet):
             "Test Re":torchmetrics.Recall(C_2,mdmc_average=A,average=M),
             "Test Pr":torchmetrics.Precision(C_2,mdmc_average=A,average=M),
             "Test F1":torchmetrics.FBetaScore(C_2,mdmc_average=A,average=M)})
+
+class AHNetPL(pl.LightningModule,AHNet):
+    def __init__(
+        self,
+        image_key: str="image",
+        label_key: str="label",
+        learning_rate: float=0.001,
+        batch_size: int=4,
+        weight_decay: float=0.005,
+        training_dataloader_call: Callable=None,
+        loss_fn: Callable=torch.nn.functional.binary_cross_entropy,
+        loss_params: dict={},*args,**kwargs) -> torch.nn.Module:
+        """Standard AHNet [1] implementation for Pytorch Lightning.
+
+        Args:
+            image_key (str): key corresponding to the key from the train
+            dataloader.
+            learning_rate (float, optional): learning rate. Defaults to 0.001.
+            batch_size (int, optional): batch size. Defaults to 4.
+            weight_decay (float, optional): weight decay for optimizer. Defaults 
+            to 0.005.
+            training_dataloader_call (Callable, optional): call for the 
+            training dataloader. Defaults to None.
+            loss_fn (Callable, optional): function to calculate the loss. 
+            Defaults to torch.nn.functional.binary_cross_entropy.
+            loss_params (dict, optional): additional parameters for the loss
+            function. Defaults to {}.
+            args: arguments for AHNet class.
+            kwargs: keyword arguments for AHNet class.
+
+        [1] https://link.springer.com/chapter/10.1007/978-3-030-00934-2_94
+
+        Returns:
+            pl.LightningModule: a AHNet module.
+        """
+        
+        pl.LightningModule.__init__(self)
+        AHNet.__init__(self,*args,**kwargs)
+        
+        self.image_key = image_key
+        self.label_key = label_key
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.weight_decay = weight_decay
+        self.training_dataloader_call = training_dataloader_call
+        self.loss_fn = loss_fn
+        self.loss_params = loss_params
+        
+        self.setup_metrics()
+        
+        self.loss_accumulator = 0.
+        self.loss_accumulator_d = 0.
+   
+    def calculate_loss(self,prediction,y,weights=None):
+        loss = self.loss_fn(prediction,y,**self.loss_params)
+        return loss.mean()
+    
+    def training_step(self,batch,batch_idx):
+        x, y = batch[self.image_key],batch[self.label_key]
+        prediction = self.forward(x)
+        prediction = torch.squeeze(prediction,1)
+        y = torch.squeeze(y,1)
+        batch_size = int(prediction.shape[0])
+        if batch_size == 1:
+            y = torch.unsqueeze(y,0)
+        y = torch.squeeze(y,1)
+
+        loss = self.calculate_loss(prediction,y)
+            
+        self.log("train_loss", loss)
+        prediction = prediction
+        try: y = torch.round(y).int()
+        except: pass
+        for k in self.train_metrics:
+            self.train_metrics[k](prediction,y)
+            self.log(
+                k,self.train_metrics[k],on_epoch=True,
+                on_step=False,prog_bar=True)
+        return loss
+    
+    def validation_step(self,batch,batch_idx):
+        x, y = batch[self.image_key],batch[self.label_key]
+        prediction = self.forward(x)
+        prediction = torch.squeeze(prediction,1)
+        y = torch.squeeze(y,1)
+        batch_size = int(prediction.shape[0])
+        if batch_size == 1:
+            y = torch.unsqueeze(y,0)
+        y = torch.squeeze(y,1)
+
+        loss = self.calculate_loss(prediction,y,None)
+
+        self.loss_accumulator += loss
+        self.loss_accumulator_d += 1.
+        try: y = torch.round(y).int()
+        except: pass
+        for k in self.val_metrics:
+            self.val_metrics[k].update(prediction,y)        
+        return loss
+
+    def test_step(self,batch,batch_idx):
+        x, y = batch[self.image_key],batch[self.label_key]
+        prediction = self.forward(x)
+        prediction = torch.squeeze(prediction,1)
+        y = torch.squeeze(y,1)
+        batch_size = int(prediction.shape[0])
+        if batch_size == 1:
+            y = torch.unsqueeze(y,0)
+        y = torch.squeeze(y,1)
+                
+        loss = self.calculate_loss(prediction,y,None)
+
+        self.loss_accumulator += loss
+        self.loss_accumulator_d += 1.
+        try: y = torch.round(y).int()
+        except: pass
+        for k in self.test_metrics:
+            self.test_metrics[k].update(prediction,y)        
+        return loss
+
+    def train_dataloader(self) -> torch.utils.data.DataLoader:
+        return self.training_dataloader_call()
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(
+            self.parameters(),lr=self.learning_rate,
+            weight_decay=self.weight_decay)
+        lr_schedulers = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer,patience=5,min_lr=1e-6,factor=0.2)
+
+        return {"optimizer":optimizer,
+                "lr_scheduler":lr_schedulers,
+                "monitor":"val_loss"}
+    
+    def on_validation_epoch_end(self):
+        for k in self.val_metrics: 
+            val = self.val_metrics[k].compute()
+            self.log(
+                k,val,prog_bar=True)
+            self.val_metrics[k].reset()
+        val_loss = self.loss_accumulator/self.loss_accumulator_d
+        sch = self.lr_schedulers().state_dict()
+        lr = self.learning_rate
+        last_lr = sch['_last_lr'][0] if '_last_lr' in sch else lr
+        self.log("lr",last_lr)
+        self.log("val_loss",val_loss,prog_bar=True)
+        self.loss_accumulator = 0.
+        self.loss_accumulator_d = 0.
+
+    def setup_metrics(self):
+        if self.n_classes == 2:
+            C_1 = 2
+            C_2 = None
+            A = None
+            M = "micro"
+        else:
+            C_1 = self.n_classes
+            C_2 = self.n_classes
+            A = "samplewise"
+            M = "macro"
+        self.train_metrics = torch.nn.ModuleDict({
+            "IoU":torchmetrics.JaccardIndex(C_1,mdmc_average=A),
+            "Re":torchmetrics.Recall(C_2,mdmc_average=A,average=M),
+            "Pr":torchmetrics.Precision(C_2,mdmc_average=A,average=M),
+            "F1":torchmetrics.FBetaScore(C_2,mdmc_average=A,average=M)})
+        self.val_metrics = torch.nn.ModuleDict({
+            "VIoU":torchmetrics.JaccardIndex(C_1,mdmc_average=A),
+            "VRe":torchmetrics.Recall(C_2,mdmc_average=A,average=M),
+            "VPr":torchmetrics.Precision(C_2,mdmc_average=A,average=M),
+            "VF1":torchmetrics.FBetaScore(C_2,mdmc_average=A,average=M)})
+        self.test_metrics = torch.nn.ModuleDict({
+            "Test IoU":torchmetrics.JaccardIndex(C_1,mdmc_average=A),
+            "Test Re":torchmetrics.Recall(C_2,mdmc_average=A,average=M),
+            "Test Pr":torchmetrics.Precision(C_2,mdmc_average=A,average=M),
+            "Test F1":torchmetrics.FBetaScore(C_2,mdmc_average=A,average=M)})
