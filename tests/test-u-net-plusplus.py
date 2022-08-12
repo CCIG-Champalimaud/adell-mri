94fdfe2f887289ae5f1d444251369de58c1f46a3
josegcpa <jose.almeida@research.fchampalimaud.org>
2022-08-12 14:55:05 +0100

    updated unet++ test

--- tests/test-u-net-plusplus.py
+++ tests/test-u-net-plusplus.py
@@ -18,9 +18,36 @@ for D in depths:
                 i = torch.rand(size=[1,c,h,w])
             elif sd == 3:
                 i = torch.rand(size=[1,c,h,w,d])
-            a = UNetPlusPlus(sd,depth=D,upscale_type="transpose",padding=1,
-                    strides=S,kernel_sizes=K,conv_type=conv_type)
-            o,o_aux = a(i,return_aux=True)
+            a = UNetPlusPlus(
+                sd,depth=D,upscale_type="transpose",padding=1,
+                strides=S,kernel_sizes=K,conv_type=conv_type,
+                bottleneck_classification=True)
+            o,o_aux,bb = a(i,return_aux=True)
+            print("\tOutput shape:")
+            print("\t\t{}".format(o.shape))
+            print("\tAux. output shape:")
+            for x in o_aux:
+                print("\t\t{}".format(x.shape))
+
+for D in depths:
+    K = [3 for _ in D]
+    S = [2 for _ in D]
+    for sd in spatial_dims:
+        for conv_type in ["regular","resnet"]:
+            print("Testing {}D with depth={} and conv_type={} and skip_layer_conditioning".format(
+                sd,D,conv_type))
+            if sd == 2:
+                i = torch.rand(size=[1,c,h,w])
+                i_skip = torch.rand(size=[1,c,h,w])
+            elif sd == 3:
+                i = torch.rand(size=[1,c,h,w,d])
+                i_skip = torch.rand(size=[1,c,h,w,d])
+            a = UNetPlusPlus(
+                sd,depth=D,upscale_type="transpose",padding=1,
+                strides=S,kernel_sizes=K,conv_type=conv_type,
+                bottleneck_classification=True,
+                skip_conditioning=1)
+            o,o_aux,bb = a(i,return_aux=True,X_final_layer=i_skip)
             print("\tOutput shape:")
             print("\t\t{}".format(o.shape))
             print("\tAux. output shape:")
