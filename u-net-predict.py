a5f786c1966de95fa7c83f3657944d9d320378e0
josegcpa <jose.almeida@research.fchampalimaud.org>
2022-09-15 17:02:03 +0100

    fixed bugs in u-net-predict

--- u-net-predict.py
+++ u-net-predict.py
@@ -11,9 +11,10 @@ from skimage import filters
 from tqdm import tqdm
 
 from lib.utils import (
-    ConditionalRescalingd,split)
+    ConditionalRescalingd,split,resample_image)
 from lib.modules.layers import ResNet
-from lib.modules.segmentation import UNet,UNetPlusPlus
+from lib.modules.segmentation import UNet
+from lib.modules.segmentation_plus import UNetPlusPlus
 from lib.modules.config_parsing import parse_config_unet,parse_config_ssl
 
 if __name__ == "__main__":
@@ -21,29 +22,29 @@ if __name__ == "__main__":
 
     # data
     parser.add_argument(
-        '--image_paths',dest='image_paths',type=str,
+        '--image_paths',dest='image_paths',type=str,nargs="+",
         help="Paths to input images (if more than one input sequence, these \
             should be comma-separated)",required=True)
     parser.add_argument(
-        '--resize_keys',dest='resize_keys',type=str,nargs='+',default=None,
+        '--resize_keys',dest='resize_keys',type=int,nargs='+',default=None,
         help="Keys that will be resized to input size")
     parser.add_argument(
         '--target_spacing',dest='target_spacing',action="store",default=None,
         help="Resamples all images to target spacing",nargs='+',type=float)
     parser.add_argument(
-        '--input_idx',dest='input_idx',type=str,nargs='+',
+        '--input_idx',dest='input_idx',type=int,nargs='+',
         help="Indices for images which will be input",
         required=True)
     parser.add_argument(
-        '--skip_idx',dest='skip_idx',type=str,default=None,
+        '--skip_idx',dest='skip_idx',type=int,default=None,
         nargs='+',
         help="Indices for images used for skip connection conditioning.")
     parser.add_argument(
-        '--mask_idx',dest='mask_idx',type=str,nargs='+',
+        '--mask_idx',dest='mask_idx',type=int,nargs='+',
         help="Indices for images which are masks (changes interpolation)",
         default=None)
     parser.add_argument(
-        '--adc_image_idx',dest='adc_image_idx',type=str,nargs='+',
+        '--adc_idx',dest='adc_idx',type=int,nargs='+',
         help="Indices for ADC images (normalized differently)",
         default=None)
     parser.add_argument(
@@ -70,20 +71,22 @@ if __name__ == "__main__":
     parser.add_argument(
         '--unet_pp',dest='unet_pp',action="store_true",
         help="Uses U-Net++ rather than U-Net")
-    parser.add_argument('n_classes',dest='n_classes',type=int,
+    parser.add_argument(
+        '--n_classes',dest='n_classes',type=int,
         help='Number of classes')
     parser.add_argument(
         '--res_config_file',dest='res_config_file',action="store",default=None,
         help="Uses a ResNet as a backbone (depths are inferred from this). \
             This config file is then used to parameterise the ResNet.")
     parser.add_argument(
-        '--checkpoint',dest='checkpoint',action="store",
+        '--checkpoints',dest='checkpoints',action="store",nargs="+",
         help="Checkpoint for network")
     
     # inference
     parser.add_argument(
         '--dev',dest='dev',type=str,
-        help="Device for PyTorch training",choices=["cuda","cpu"])
+        help="Device for PyTorch training",
+        choices=["cuda","cuda:0","cuda:1","cpu"])
     parser.add_argument(
         '--n_workers',dest='n_workers',
         help="Number of workers",default=1,type=int)
@@ -100,13 +103,16 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    torch.manual_seed(args.seed)
-    random.seed(args.seed)
-    np.random.seed(args.seed)
-    g = torch.Generator()
-    g.manual_seed(args.seed)
-
     n_classes = args.n_classes
+
+    if args.features is None:
+        args.features = ["" for _ in args.image_paths]
+    if args.mask_idx is None:
+        args.mask_idx = []
+    if args.skip_idx is None:
+        args.skip_idx = []
+    if args.adc_idx is None:
+        args.adc_idx = []
 
     data_dict = {}
     keys = []
@@ -123,18 +129,18 @@ if __name__ == "__main__":
             if k not in keys:
                 keys.append(k)
                 if i in args.mask_idx:
-                    interpolation.append("nearest")
                     interpolation_resample.append("nearest")
                 else:
-                    interpolation.append("area")
                     interpolation_resample.append("bilinear")
-        for i,im in enumerate(feature_set):
+        for i,im in enumerate(feature_set.split(",")):
+            k = "feature_{}".format(i)
             if k not in feature_keys:
                 feature_keys.append(k)
-            data_dict[identifier]["feature_{}".format(i)] = im
+            data_dict[identifier][k] = float(im)
 
     input_keys = [x for i,x in enumerate(keys) if i in args.input_idx]
     aux_keys = [x for i,x in enumerate(keys) if i in args.skip_idx]
+
     if len(aux_keys) > 0:
         aux_key_net = "aux_key"
     else:
@@ -146,9 +152,9 @@ if __name__ == "__main__":
         args.input_size = [round(x) for x in args.input_size]
 
     network_config,loss_key = parse_config_unet(
-        args.config_file,len(keys),n_classes)
+        args.config_file,len(input_keys),n_classes)
 
-    def get_transforms(x,label_mode=None):
+    def get_transforms():
         if args.target_spacing is not None:
             rs = [
                 monai.transforms.Spacingd(
@@ -166,13 +172,6 @@ if __name__ == "__main__":
             scaling_ops.append(
                 monai.transforms.ScaleIntensityd(
                     adc_input_keys,None,None,1-args.adc_factor))
-        if args.input_size is not None and len(args.resize_keys) > 0:
-            intp_ = [k for k,kk in zip(interpolation,keys) 
-                     if kk in args.resize_keys]
-            resize = [monai.transforms.Resized(
-                args.resize_keys,tuple(args.input_size),mode=intp_)]
-        else:
-            resize = []
         if args.crop_size is not None:
             crop_op = [
                 monai.transforms.CenterSpatialCropd(
@@ -196,27 +195,35 @@ if __name__ == "__main__":
                     func=lambda x:np.reshape(x,[1])),
                 monai.transforms.ConcatItemsd(
                     feature_keys,"tabular_features")]
+        else:
+            feature_concat = []
         return [
                 monai.transforms.LoadImaged(keys),
                 monai.transforms.AddChanneld(keys),
                 monai.transforms.Orientationd(keys,"RAS"),
                 *rs,
-                *resize,
                 *crop_op,
                 *scaling_ops,
-                monai.transforms.EnsureTyped(keys),
-                monai.transforms.ConcatItemsd(keys,"image"),
+                monai.transforms.ConcatItemsd(input_keys,"image"),
                 *aux_concat,
                 *feature_concat,
-                monai.transforms.ToTensord(["image"])]
+                monai.transforms.ToTensord(
+                    ["image",aux_key_net,"tabular_features"],
+                    allow_missing_keys=True),
+                monai.transforms.EnsureTyped(
+                    ["image",aux_key_net,"tabular_features"],
+                    device=args.dev,allow_missing_keys=True),]
 
-    label_mode = "binary" if n_classes == 2 else "cat"
     all_pids = [k for k in data_dict]
 
     print("Setting up networks...")
+    for k in ['weight_decay','learning_rate','batch_size','loss_fn']:
+        if k in network_config:
+            del network_config[k]
+
     if args.res_config_file is not None:
         _,network_config_ssl = parse_config_ssl(
-            args.res_config_file,0.,len(keys),network_config["batch_size"])
+            args.res_config_file,0.,len(input_keys),1)
         for k in ['weight_decay','learning_rate','batch_size']:
             if k in network_config_ssl:
                 del network_config_ssl[k]
@@ -239,6 +246,8 @@ if __name__ == "__main__":
         all_params = {
             "mean":torch.zeros([len(feature_keys)]),
             "std":torch.ones([len(feature_keys)])}
+    else:
+        all_params = None
 
     if args.unet_pp == True:
         unet = UNetPlusPlus(
@@ -246,60 +255,92 @@ if __name__ == "__main__":
             n_classes=n_classes,
             bottleneck_classification=False,
             skip_conditioning=len(aux_keys),
-            feature_conditioning=len(args.feature_keys),
+            feature_conditioning=len(feature_keys),
             feature_conditioning_params=all_params,
-            **network_config)
+            **network_config).to(args.dev)
     else:
         unet = UNet(
             encoding_operations=encoding_operations,
             n_classes=n_classes,
             bottleneck_classification=False,
             skip_conditioning=len(aux_keys),
-            feature_conditioning=len(args.feature_keys),
+            feature_conditioning=len(feature_keys),
             feature_conditioning_params=all_params,
-            **network_config)
-
-    state_dict = torch.load(
-        args.checkpoint,map_location=args.dev)['state_dict']
-    inc = unet.load_state_dict(state_dict)
+            **network_config).to(args.dev)
+        unet.eval()
     
-    print(inc)
-
     print("Setting up data...")
-    transforms = get_transforms()
+    transforms = monai.transforms.Compose(get_transforms())
+
+    checkpoint_outputs = {}
+    for checkpoint in args.checkpoints:     
+        state_dict = torch.load(
+            checkpoint,map_location=args.dev)['state_dict']
+        inc = unet.load_state_dict(state_dict)
+        print(inc)
+        for k in tqdm(data_dict):
+            d = transforms(data_dict[k])
+
+            X = torch.unsqueeze(d["image"],0)
+            x_cond = None
+            x_fc = None
+            if len(aux_keys) > 0:
+                x_cond = torch.unsqueeze(d[aux_key_net],0)
+            if len(feature_keys) > 0:
+                x_fc = torch.unsqueeze(d["tabular_features"],0)
                 
-    for k in tqdm(data_dict):
-        d = transforms(data_dict[k])
+            if args.tta == True:
+                output,_ = unet.forward(
+                    X,X_skip_layer=x_cond,
+                    X_feature_conditioning=x_fc)
+                outputs = [output[0].cpu().detach().numpy()]
+                for d in [(2,),(3,),(4,)]:
+                    flipped_X = torch.flip(X,d)
+                    if x_cond is not None:
+                        flipped_X_cond = torch.flip(x_cond,d) 
+                    else:
+                        flipped_X_cond = None
+                    output,_ = unet.forward(
+                        flipped_X,X_skip_layer=flipped_X_cond,
+                        X_feature_conditioning=x_fc)
+                    output = torch.flip(output,d)[0].cpu().detach().numpy()
+                    outputs.append(output)
 
-        X = torch.unsqueeze(d["image"],0)
-        x_cond = None
-        x_fc = None
-        if len(aux_keys) is not None:
-            x_cond = torch.unsqueeze(d[aux_key_net],0)
-        if len(feature_keys) is not None:
-            x_fc = torch.unsqueeze(d["tabular_features"],0)            
+                output = sum(outputs)/len(outputs)
+            else:
+                output,_ = unet.forward(
+                        X,X_skip_layer=x_cond,X_feature_conditioning=x_fc)
+                output = output[0].cpu().detach().numpy()
 
-        if args.tta == True:
-            X = torch.cat([X,X[:,:,::-1]])
-            x_cond = torch.cat([x_cond,x_cond[:,:,::-1]])
+            if k not in checkpoint_outputs:
+                checkpoint_outputs[k] = []
+            checkpoint_outputs[k].append(output)
 
-        output = unet.forward(
-            X,X_skip_layer=x_cond,X_feature_conditioning=x_fc)
-        if args.tta == True:
-            output = split(output,2,0)
-            output = (output[0] + output[1])/2
-        output = output.squeeze(0)
-        output = output.detach().cpu().numpy()
+    for k in checkpoint_outputs:
+        outputs = checkpoint_outputs[k]
+        output = sum(outputs)/len(outputs)
         if n_classes == 2:
             output = filters.apply_hysteresis_threshold(
-                output,0.45, 0.5)[0]
+                output, 0.5, 0.5)[0].astype(np.int16)
         else:
             output = np.argmax(output,axis=0)
-        
-        target_image = sitk.ReadImage(data_dict[k]["image"])
-        output = sitk.GetImageFromArray(output)
+                    
+        target_image = sitk.ReadImage(data_dict[k]["image_0"])
+        target_spacing = target_image.GetSpacing()
+        target_size = target_image.GetSize()
+        padding_size = np.multiply(
+            target_size,
+            np.array(target_spacing)/np.array(args.target_spacing)).astype(np.uint32)
+        output = monai.transforms.SpatialPad(padding_size)(output[np.newaxis])[0]
+        output = sitk.GetImageFromArray(output.swapaxes(0,2)[:,::-1,::-1])
+        output.SetSpacing(args.target_spacing)
+        output.SetDirection(target_image.GetDirection())
+        output.SetOrigin(target_image.GetOrigin())
+        output = resample_image(
+            output,target_image.GetSpacing(),target_image.GetSize(),True)
         output.CopyInformation(target_image)
         output = sitk.Cast(output,sitk.sitkInt16)
+        os.makedirs(args.output_dir,exist_ok=True)
         sitk.WriteImage(
             output,
-            os.path.join(args.output_path,"{}.mha".format(k)))
+            os.path.join(args.output_dir,"{}.nii.gz".format(k)))
