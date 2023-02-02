a1c8823f69bec6d8b36afb293456b5de1b4b8b61
josegcpa <jose.almeida@research.fchampalimaud.org>
2023-02-02 18:44:44 +0000

    added layernorm with support for channels first format

--- lib/modules/layers/regularization.py
+++ lib/modules/layers/regularization.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 
 class UOut(torch.nn.Module):
     def __init__(self,beta: float=0.) -> torch.nn.Module:
@@ -43,3 +44,33 @@ class UOut(torch.nn.Module):
             return X
         else:
             return X
+
+class LayerNorm(torch.nn.Module):
+    # from: https://github.com/facebookresearch/VICRegL/blob/main/convnext.py
+    r""" LayerNorm that supports two data formats: channels_last (default) or channels_first. 
+    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with 
+    shape (batch_size, height, width, channels) while channels_first corresponds to inputs 
+    with shape (batch_size, channels, height, width).
+    """
+
+    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_last"):
+        super().__init__()
+        self.weight = torch.nn.Parameter(torch.ones(normalized_shape))
+        self.bias = torch.nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+        self.data_format = data_format
+        if self.data_format not in ["channels_last", "channels_first"]:
+            raise NotImplementedError
+        self.normalized_shape = (normalized_shape,)
+
+    def forward(self, x):
+        if self.data_format == "channels_last":
+            return F.layer_norm(
+                x, self.normalized_shape, self.weight, self.bias, self.eps
+            )
+        elif self.data_format == "channels_first":
+            u = x.mean(1, keepdim=True)
+            s = (x - u).pow(2).mean(1, keepdim=True)
+            x = (x - u) / torch.sqrt(s + self.eps)
+            x = self.weight[:, None, None] * x + self.bias[:, None, None]
+            return x
