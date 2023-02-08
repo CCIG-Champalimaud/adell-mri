63975db563ce8683d62381a8f42a8b10c316217f
josegcpa <jose.almeida@research.fchampalimaud.org>
2023-02-08 15:39:30 +0000

    added functionalities to export ssl model as torchscript and testing it

---
+++ ssl/selfsl-2d-model-to-torchscript.py
@@ -0,0 +1,112 @@
+import sys
+sys.path.append(r"..")
+from lib.modules.self_supervised.pl import (
+    UNet,ConvNeXt,ResNet)
+from lib.modules.config_parsing import parse_config_ssl,parse_config_unet
+import inspect
+from copy import deepcopy
+import argparse
+import torch
+
+torch.backends.cudnn.benchmark = True
+
+def force_cudnn_initialization():
+    """Convenience function to initialise CuDNN (and avoid the lazy loading
+    from PyTorch).
+    """
+    s = 16
+    dev = torch.device('cuda')
+    torch.nn.functional.conv2d(torch.zeros(s,s,s,s,device=dev), 
+                               torch.zeros(s,s,s,s,device=dev))
+
+def filter_dicom_dict_on_presence(data_dict,all_keys):
+    def check_intersection(a,b):
+        return len(set.intersection(set(a),set(b))) == len(set(b))
+    for k in data_dict:
+        for kk in data_dict[k]:
+            data_dict[k][kk] = [
+                element for element in data_dict[k][kk]
+                if check_intersection(element.keys(),all_keys)]
+    return data_dict
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Converts a SelfSL model to torchscript")
+
+    # network + training
+    parser.add_argument(
+        '--net_type',dest='net_type',
+        choices=["resnet","unet_encoder","convnext"],
+        help="Which network should be trained.")
+    parser.add_argument(
+        "--input_shape",dest="input_shape",required=True,
+        help="Unbatched input shape",type=int,nargs="+")
+    parser.add_argument(
+        '--config_file',dest="config_file",
+        help="Path to network configuration file (yaml)",
+        required=True)
+    parser.add_argument(
+        '--checkpoint',dest='checkpoint',action="store",
+        help="Loads this checkpoint")
+    parser.add_argument(
+        '--output_model_path',dest='output_model_path',required=True,
+        help="Output path the .pt model")
+    
+    # training
+    parser.add_argument(
+        '--dev',dest='dev',type=str,default="cuda",
+        help="Device for model allocation")
+    parser.add_argument(
+        '--vicreg',dest='vicreg',action="store_true",
+        help="Use VICReg loss")
+    parser.add_argument(
+        '--vicregl',dest='vicregl',action="store_true",
+        help="Use VICRegL loss")
+
+    args = parser.parse_args()
+    
+    if args.net_type == "unet_encoder":
+        network_config,_ = parse_config_unet(
+            args.config_file,args.input_shape[0],2)
+        network_config_correct = deepcopy(network_config)
+        for k in network_config:
+            if k in ["loss_fn"]:
+                del network_config_correct[k]
+    else:
+        network_config,network_config_correct = parse_config_ssl(
+            args.config_file,0.0,args.input_shape[0])
+
+    if args.net_type == "unet_encoder":
+        network_config_correct["encoder_only"] = True
+        fn_args = [k for k in inspect.signature(UNet).parameters]
+        network_config_correct = {k:network_config_correct[k] 
+                                  for k in network_config_correct
+                                  if k in fn_args}
+        ssl = UNet(**network_config_correct)
+    elif args.net_type == "convnext":
+        fn_args = [k for k in inspect.signature(ConvNeXt).parameters]
+        network_config_correct = {k:network_config_correct[k] 
+                                  for k in network_config_correct
+                                  if k in fn_args}
+        network_config_correct["backbone_args"] = {
+            k:network_config_correct["backbone_args"][k] 
+            for k in network_config_correct["backbone_args"]
+            if k not in ["res_type"]}
+        ssl = ConvNeXt(**network_config_correct)
+    else:
+        fn_args = [k for k in inspect.signature(ResNet).parameters]
+        network_config_correct = {k:network_config_correct[k] 
+                                  for k in network_config_correct
+                                  if k in fn_args}
+        ssl = ResNet(**network_config_correct)
+
+    ssl = ssl.to(args.dev)
+    state_dict = torch.load(
+        args.checkpoint,
+        map_location=args.dev.split(":")[0])['state_dict']
+    inc = ssl.load_state_dict(state_dict)
+    
+    example = torch.rand(1,*args.input_shape).to(args.dev)
+    traced_ssl = torch.jit.trace(ssl,example)
+    
+    traced_ssl.save(args.output_model_path)
