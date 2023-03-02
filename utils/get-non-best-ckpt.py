7a487c010996e991c382e6f4baf41d2bfa400fbd
josegcpa <jose.almeida@research.fchampalimaud.org>
2023-03-02 13:20:06 +0000

    added script that returns non-top models

---
+++ utils/get-non-best-ckpt.py
@@ -0,0 +1,22 @@
+import sys
+import os
+import re
+from glob import glob
+
+path = sys.argv[1]
+split = sys.argv[2]
+pattern = sys.argv[3]
+
+pat = re.compile(pattern)
+all_files = {}
+for file in glob(os.path.join(path,"*best*")):
+    root,rest = file.split(split)
+    if root not in all_files:
+        all_files[root] = []
+    value = float(pat.search(file).group())
+    all_files[root].append([file,value])
+
+for k in all_files:
+    worst = sorted(all_files[k],key=lambda x: -x[1])[1:]
+    worst = [x[0] for x in worst]
+    print("\n".join(worst))
