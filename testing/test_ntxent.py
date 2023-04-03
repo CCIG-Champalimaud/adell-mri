d505e53f88d560d94be5e47ffe54bfae9912c162
josegcpa <jose.almeida@research.fchampalimaud.org>
2023-04-03 19:19:48 +0100

    corrected unit-test

--- testing/test_ntxent.py
+++ testing/test_ntxent.py
@@ -3,10 +3,10 @@ import os
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),'..'))
 
 import torch
-from lib.modules.self_supervised import NTXent
+from lib.modules.self_supervised import NTXentLoss
 
 def test_ntxent():
       X1,X2 = torch.rand(4,32),torch.rand(4,32)
-      ntxent = NTXent()
+      ntxent = NTXentLoss()
       p = ntxent(X1,X2)
 
