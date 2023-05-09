41a6b7c6fa9c40297e2e0e9b4e887f4710d4c8e6
josegcpa <jose.almeida@research.fchampalimaud.org>
2023-05-09 22:31:13 +0100

    expanded and factorized sitk spacing inference

--- lib/utils/sitk_utils.py
+++ lib/utils/sitk_utils.py
@@ -3,10 +3,42 @@ import numpy as np
 from multiprocess import Pool
 from tqdm import tqdm
 from typing import List
+from typing import Dict,Tuple
+from dataclasses import dataclass
 from ..custom_types import DatasetDict
 
-def read_spacing(path:str):
-    return sitk.ReadImage(path).GetSpacing()
+@dataclass
+class ReadSpacing:
+    dataset_dict: DatasetDict
+    image_key: str
+    
+    def __call__(self,key:str)->Tuple[float,float,float]:
+        sp = sitk.ReadImage(self.dataset_dict[key][self.image_key]).GetSpacing()
+        return key,sp
+
+def spacing_values_from_dataset_json(dataset_dict:DatasetDict,
+                                     key:str,
+                                     n_workers:int=1)->Dict[str,Tuple[float,float,float]]:
+    all_spacings = {}
+    read_spacing = ReadSpacing(dataset_dict,key)
+    with tqdm(dataset_dict) as pbar:
+        pbar.set_description("Inferring target spacing")
+        if n_workers > 1:
+            pool = Pool(n_workers)
+            path_iterable = pool.imap(read_spacing,dataset_dict.keys())
+        else:
+            path_iterable = map(read_spacing,dataset_dict.keys())
+        for key,spacing in path_iterable:
+            all_spacings[key] = spacing
+            pbar.update()
+    return all_spacings
+
+def get_spacing_quantile(spacing_dict:Dict[str,Tuple[float,float,float]],
+                         quantile:float=0.5):
+    all_spacings = np.array([spacing_dict[k] for k in spacing_dict])
+    output = np.quantile(all_spacings,quantile,axis=0).tolist()
+    print("Inferred spacing:",output)
+    return output
 
 def spacing_from_dataset_json(dataset_dict:DatasetDict,
                               key:str,
@@ -26,20 +58,9 @@ def spacing_from_dataset_json(dataset_dict:DatasetDict,
     Returns:
         List[float]: spacing at the specified quantile.
     """
-    all_spacings = []
-    all_paths = [dataset_dict[k][key] for k in dataset_dict
-                 if key in dataset_dict[k]]
-    with tqdm(all_paths) as pbar:
-        pbar.set_description("Inferring target spacing")
-        if n_workers > 1:
-            pool = Pool(n_workers)
-            path_iterable = pool.imap(read_spacing,all_paths)
-        else:
-            path_iterable = map(read_spacing,all_paths)
-        for spacing in path_iterable:
-            all_spacings.append(spacing)
-            pbar.update()
-    all_spacings = np.array(all_spacings)
-    output = np.quantile(all_spacings,quantile,axis=0).tolist()
-    print("Inferred spacing:",output)
+    spacing_values = spacing_values_from_dataset_json(
+        dataset_dict=dataset_dict,
+        key=key,
+        n_workers=n_workers)
+    output = get_spacing_quantile(spacing_values,quantile)
     return output
