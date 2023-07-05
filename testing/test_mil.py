25eb73fcd153f9cb577b1093406a43a6cceb050a
josegcpa <jose.almeida@research.fchampalimaud.org>
2023-07-05 23:24:28 +0100

    better test for mil

--- testing/test_mil.py
+++ testing/test_mil.py
@@ -10,7 +10,7 @@ from lib.modules.classification.classification.multiple_instance_learning import
 batch_size = 4
 input_dim = 1
 input_dim_mil = 32
-classification_structure = [32,32]
+classification_structure = [32,16]
 n_slices = 8
 n_classes = 2
 adn_fn = get_adn_fn(1,"identity","gelu",0.1)
@@ -24,6 +24,7 @@ def test_transformer(classification_mode):
         classification_mode=classification_mode,
         classification_adn_fn=adn_fn,
         n_slices=n_slices,use_positional_embedding=False,
+        attention=True,
         dim=2)
     input_tensor = torch.rand(batch_size,input_dim,32,32,n_slices)
     output = mod(input_tensor)
