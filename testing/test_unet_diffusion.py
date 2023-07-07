c60ab3779fbfabe69f38b195a7fd2f5d0fbb1c2d
josegcpa <jose.almeida@research.fchampalimaud.org>
2023-07-07 08:38:25 +0100

    added timestep to test

--- testing/test_unet_diffusion.py
+++ testing/test_unet_diffusion.py
@@ -10,6 +10,7 @@ h,w,d,c = 32,32,20,1
 
 depths = [[16,32,64],[16,32,64,128]]
 spatial_dims = [2,3]
+t = torch.Tensor([4])
 
 param_list = []
 for dim in spatial_dims:
@@ -37,7 +38,7 @@ def test_unet_base(D,sd,conv_type,strides):
     a = DiffusionUNet(
         spatial_dimensions=sd,depth=D,upscale_type="transpose",padding=1,n_channels=c,
         strides=S,kernel_sizes=K,conv_type=conv_type,link_type="identity")
-    o = a(i)
+    o = a(i,t)
     assert list(o.shape) == output_size
 
 @pytest.mark.parametrize("D,sd,conv_type,strides",param_list)
@@ -51,7 +52,7 @@ def test_unet_class_base(D,sd,conv_type,strides):
     else:
         S = [2 for _ in D]
     
-    cls = torch.rand(size=[1,2])
+    cls = torch.randint(0,2,size=[1])
     if sd == 2:
         i = torch.rand(size=[1,c,h,w])
         output_size = [1,c,h,w]
@@ -62,5 +63,5 @@ def test_unet_class_base(D,sd,conv_type,strides):
         spatial_dimensions=sd,depth=D,upscale_type="transpose",padding=1,n_channels=c,
         strides=S,kernel_sizes=K,conv_type=conv_type,link_type="identity",
         classifier_free_guidance=True)
-    o = a(i,cls)
+    o = a(i,t,cls)
     assert list(o.shape) == output_size
