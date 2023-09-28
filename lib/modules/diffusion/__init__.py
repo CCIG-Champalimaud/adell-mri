14063dc8ea518b8e404edb6d69d5cc12f0664fec
josegcpa <jose.almeida@research.fchampalimaud.pt>
2023-09-28 19:59:27 +0100

    added comment noting the pedagogical purposes of my diffusion implementations

--- lib/modules/diffusion/__init__.py
+++ lib/modules/diffusion/__init__.py
@@ -1,2 +1,6 @@
+# the diffusion_process and unet modules were implemented mostly to learn
+# about diffusion processes. the `pl` module is a wrapper around the very
+# good MONAI generative package
+
 from .diffusion_process import *
 from .unet import *
