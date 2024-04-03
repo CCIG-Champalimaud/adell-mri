6be686a3c85e9a01b52bfbaf4f84e6fb2e9b8167
josegcpa <jose.almeida@research.fchampalimaud.org>
2024-04-03 17:40:56 +0100

    added cropping to 2d dicom feature prediction

--- adell_mri/entrypoints/ssl/predict_folder.py
+++ adell_mri/entrypoints/ssl/predict_folder.py
@@ -10,6 +10,48 @@ from ...entrypoints.assemble_args import Parser
 torch.set_num_threads(8)
 
 
+def crop_or_pad(img: np.ndarray, target_size: tuple[int, int]) -> np.ndarray:
+    """
+    Crops or pads an image to a target size.
+
+    Args:
+      img: Input image as a numpy array.
+      target_size: Tuple of target height and width.
+
+    Returns:
+      Image cropped or padded to target size.
+    """
+
+    h, w = img.shape[2:]
+
+    new_h = target_size[0]
+    new_w = target_size[1]
+
+    if h > new_h:
+        # Crop height
+        start = (h - new_h) // 2
+        img = img[:, :, start : start + new_h, :]
+    elif h < new_h:
+        # Pad height
+        pad_h = new_h - h
+        pad_top = pad_h // 2
+        pad_bottom = new_h - pad_top
+        img = F.pad(img, (pad_top, pad_bottom, 0, 0), mode="constant")
+
+    if w > new_w:
+        # Crop width
+        start = (w - new_w) // 2
+        img = img[:, :, :, start : start + new_w]
+    elif w < new_w:
+        # Pad width
+        pad_w = new_w - w
+        pad_left = pad_w // 2
+        pad_right = new_w - pad_left
+        img = F.pad(img, (0, 0, pad_left, pad_right), mode="constant")
+
+    return img
+
+
 def main(arguments):
     parser = Parser()
 
@@ -40,6 +82,12 @@ def main(arguments):
         type=int,
         help="Target size for DICOM file resizing",
     )
+    parser.add_argument(
+        "--crop_size",
+        nargs="+",
+        type=int,
+        help="Target size for DICOM file cropping",
+    )
 
     args = parser.parse_args(arguments)
 
@@ -65,6 +113,9 @@ def main(arguments):
                 mode="bilinear",
                 align_corners=False,
             )
+
+        if args.crop_size is not None:
+            t = crop_or_pad(t, args.crop_size)
         f = (
             model(t)
             .flatten(start_dim=2)
