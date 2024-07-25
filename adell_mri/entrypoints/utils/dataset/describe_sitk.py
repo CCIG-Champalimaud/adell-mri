8d9cfd294b750c1f12d27a1a43058e093405560b
josegcpa <jose.almeida@research.fchampalimaud.pt>
2024-07-25 15:28:10 +0100

    added unique values to description

--- adell_mri/entrypoints/utils/dataset/describe_sitk.py
+++ adell_mri/entrypoints/utils/dataset/describe_sitk.py
@@ -1,4 +1,5 @@
 import argparse
+import numpy as np
 import SimpleITK as sitk
 
 desc = "Describes SITK image properties."
@@ -18,6 +19,8 @@ def main(arguments):
 
     for sitk_image_path in args.sitk_paths:
         image = sitk.ReadImage(sitk_image_path)
+        image_array = sitk.GetArrayFromImage(image)
+        u, c = np.unique(image_array, return_counts=True)
         print(f"Image: {sitk_image_path}")
         print(f"\tImage size: {image.GetSize()}")
         print(f"\tImage spacing: {image.GetSpacing()}")
@@ -27,3 +30,7 @@ def main(arguments):
             f"\tImage number of components: {image.GetNumberOfComponentsPerPixel()}"
         )
         print(f"\tImage pixel type: {image.GetPixelIDTypeAsString()}")
+        if u.shape[0] > 5:
+            print(f"\tNumber of unique values: {len(u)}")
+        else:
+            print(f"\tUnique values: {[(i,j) for i,j in zip(u,c)]}")
