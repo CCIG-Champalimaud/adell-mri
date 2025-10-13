c9a2163f03f82b5058c0052529d08793282ff431
josegcpa <jose.almeida@research.fchampalimaud.org>
2025-10-13 15:19:44 +0100

    added torchvision-like make_grid

--- adell_mri/utils/logging.py
+++ adell_mri/utils/logging.py
@@ -3,16 +3,40 @@ Includes a logging class which can store data in different formats.
 """
 
 import os
+import torch
 from dataclasses import dataclass
 from typing import Any
 
-try:
-    import pandas as pd
-except Exception:
-    raise ImportError(
-        "Pandas is required to parse parquet files. ",
-        "Please install it with `pip install pandas`.",
-    )
+
+def make_grid(image_tensor: torch.Tensor) -> torch.Tensor:
+    """
+    Creates a grid of images from a batch of images.
+
+    Args:
+        image_tensor (torch.Tensor): A tensor of shape (batch_size, channels,
+            height, width).
+
+    Returns:
+        torch.Tensor: A tensor of shape (channels, height, width) containing
+            the grid.
+    """
+    sh = image_tensor.shape
+    b, c, h, w = sh[0], sh[1], sh[2], sh[3]
+    side_x = int(b**0.5)
+    grid = []
+    for i in range(b):
+        if i % side_x == 0:
+            grid.append([])
+        grid[-1].append(image_tensor[i])
+    out_size = len(grid[0])
+    for g in range(len(grid)):
+        while len(grid[g]) < out_size:
+            grid[g].append(torch.zeros(c, h, w))
+
+    output = torch.cat([torch.cat(x, -2) for x in grid], -1).detach()
+    output = (output - output.min()) / (output.max() - output.min())
+    output = output.multiply(255).to(torch.uint8)
+    return output
 
 
 @dataclass
@@ -25,6 +49,14 @@ class CSVLogger:
         overwrite (bool, optional): whether to overwrite folds in the log CSV
             file.
     """
+
+    try:
+        import pandas as pd
+    except Exception:
+        raise ImportError(
+            "Pandas is required to parse parquet files. ",
+            "Please install it with `pip install pandas`.",
+        )
 
     file_path: str
     overwrite: bool = False
